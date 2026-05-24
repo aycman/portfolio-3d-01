4d5c52b7560e2b64722642a2ec98bf3d69317e95
aycman <man.hajilou@gmail.com>
2026-05-24 12:18:36 +0200

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,26 @@
+import { Github, Linkedin, Twitter, Home, Whatsapp } from '@boxicons/react';
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='z-10 flex justify-between mb-24 pt-10 items-center'>
+          <div className="lg:text-5xl font-extrabold text-2xl">
+            SITE NAME
+          </div> 
+
+          <div className='flex gap-5'>
+            <a className='text-2xl hover:text-gray-500 duration-300' href="#"><Home /></a>
+            <a className='text-2xl hover:text-gray-500 duration-300'  href="#"><Github /></a>
+            <a className='text-2xl hover:text-gray-500 duration-300'  href="#"><Linkedin /></a>
+            <a className='text-2xl hover:text-gray-500 duration-300'  href="#"><Whatsapp /></a>
+
+            {/* <a href="#"><Twitter size="32px" color="#1DA1F2" /></a> */}
+          </div>
+
+
+          
+    </header>
+
   )
 }
 
