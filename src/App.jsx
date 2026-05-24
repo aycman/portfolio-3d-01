dcd8090755e348edc47159b7653aae3279ad249d
aycman <man.hajilou@gmail.com>
2026-05-24 06:52:17 +0200

    update readme.md

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <div  className='container mx-auto px-12 h-screen flex-col'>
+        <Header />
+    </div>
+
+  )
+}
+
+export default App
